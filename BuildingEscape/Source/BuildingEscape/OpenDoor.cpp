70b676df615138bee4f4b40957831161f239f297
Noah <sheltielover432@gmail.com>
2020-03-17 21:39:16 -0400

    Linear Interpolation

--- BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
@@ -3,6 +3,8 @@
 
 #include "OpenDoor.h"
 #include "GameFramework/Actor.h"
+
+
 // Sets default values for this component's properties
 UOpenDoor::UOpenDoor()
 {
@@ -18,10 +20,6 @@ UOpenDoor::UOpenDoor()
 void UOpenDoor::BeginPlay()
 {
 	Super::BeginPlay();
-	float MyFloat = 90.f;
-	//FRotator CurrentRotation = GetOwner()->GetActorRotation();
-    FRotator OpenDoor = {0.f, 90.f, 0.f};
-	GetOwner()->SetActorRotation(OpenDoor);
 }
 
 
@@ -30,6 +28,13 @@ void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompon
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
+	UE_LOG(LogTemp, Warning, TEXT("%s"), *GetOwner()->GetActorRotation().ToString());
+	UE_LOG(LogTemp, Warning, TEXT("Yaw is: %f"), GetOwner()->GetActorRotation().Yaw);
+
+	float CurrentYaw = GetOwner()->GetActorRotation().Yaw;
+	FRotator OpenDoor(0.f,TargetYaw, 0.f);
+	OpenDoor.Yaw = FMath::Lerp(CurrentYaw, TargetYaw, 0.02f);
+	GetOwner()->SetActorRotation(OpenDoor);	
 }
+
 
